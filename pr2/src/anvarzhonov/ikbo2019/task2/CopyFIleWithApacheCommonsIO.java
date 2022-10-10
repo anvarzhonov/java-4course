0b624274dfc00cf2d57cfd43091e82ab8e4de13b
Anvarzhonov <anvarzhonov@mail.ru>
2022-10-10 08:14:32 +0300

    Practice 2 - Java NIO

---
+++ pr2/src/anvarzhonov/ikbo2019/task2/CopyFIleWithApacheCommonsIO.java
@@ -0,0 +1,15 @@
+package anvarzhonov.ikbo2019.task2;
+
+import java.io.FileNotFoundException;
+
+/**
+ * Реализация копирования файла с помощью библиотеки Apache Commons IO
+ *
+ * @author - Anvarzhonov Z.T. IKBO-20-19 on 09.10.2022 - 23:51
+ */
+public class CopyFIleWithApacheCommonsIO implements CopyingFile {
+    @Override
+    public void execute(String sourcePath, String destinationPath) throws FileNotFoundException {
+
+    }
+}
---
+++ pr2/src/anvarzhonov/ikbo2019/task2/CopyFileWithFIleChannel.java
@@ -0,0 +1,15 @@
+package anvarzhonov.ikbo2019.task2;
+
+import java.io.FileNotFoundException;
+
+/**
+ * Реализация копирования файла с помощью FileChannel в Java NIO
+ *
+ * @author - Anvarzhonov Z.T. IKBO-20-19 on 09.10.2022 - 23:51
+ */
+public class CopyFileWithFIleChannel implements CopyingFile{
+    @Override
+    public void execute(String sourcePath, String destinationPath) throws FileNotFoundException {
+
+    }
+}
